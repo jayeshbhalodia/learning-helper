06e517267803cb4b508302b80e696761b26125d9
Kamlesh <kamlesh.lightwebs@gmail.com>
2017-11-03 10:06:02 +0530

    setup project

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
+
+
+
+// ExpressJS Configuration
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+
+
+
+// // connect mongDB
+// mongoose.connect(config.get('mongoDBURI'));
+
+// require('./config/passport')(passport);
+// require('./app/models/ib');
